40ba3d1cfd5c3e69e7310a92b2c80977f3ca953a
dam27 <91896848+WhyNot2732@users.noreply.github.com>
2026-04-29 08:51:51 +0700

    Update soal1.c 1

--- soal1.c
+++ soal1.c
@@ -1,5 +1,65 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 3
+ *   Hari dan Tanggal    : Rabu, 29 April 2026
+ *   Nama (NIM)          : R. Damar Prawiro Kusumo Sudradjat (13224109)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Membuat sistem arsip artefak yang dapat mengurutkan berdasarkan data artefak yang dimasukkan
+ * 
+ */
 
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct data
+{
+    char *nama;
+    char *kategori;
+    int tahun;
+    int nilai;
+} data;
+
+int compare(const void *a, const void *b);
+
+int main()
+{
+    int N;
+    char input[100];
+    char* tok;
+    fgets(input, 100, stdin);
+    tok = strtok(input, " ");
+    N = atoi(tok);
+    data arr[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        tok = strtok(NULL, " ");
+        arr[i].nama = (char*)malloc(strlen(tok) * sizeof(char));
+        strcpy(arr[i].nama, tok);
+
+        tok = strtok(NULL, " ");
+        arr[i].kategori = (char*)malloc(strlen(tok) * sizeof(char));
+        strcpy(arr[i].kategori, tok);
+
+        tok = strtok(NULL, " ");
+        arr[i].tahun = atoi(tok);
+
+        tok = strtok(NULL, " ");
+        arr[i].nilai = atoi(tok);
+    }
+
+
+    qsort(arr, N, sizeof(data), compare);
+    
+    for (int i = 0; i < N; i++)
+    {
+        printf("%s %s %d %d\n", arr[i].nama, arr[i].kategori, arr[i].tahun, arr[i].nilai);
+    }
+    
     return 0;
 }
+
+int compare(const void *a, const void *b){
+    return strcmp(((data *)a)->kategori, ((data *)b)->kategori);
+}
+
